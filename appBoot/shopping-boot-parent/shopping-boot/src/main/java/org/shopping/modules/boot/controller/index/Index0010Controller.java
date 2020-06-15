9c69dd399993297af0242ba151f3d165abd3400b
DH <2192406468@qq.com>
2020-06-15 10:15:14 +0800

    首次

---
+++ appBoot/shopping-boot-parent/shopping-boot/src/main/java/org/shopping/modules/boot/controller/index/Index0010Controller.java
@@ -0,0 +1,20 @@
+package org.shopping.modules.boot.controller.index;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/boot/index0010")
+public class Index0010Controller {
+
+    @GetMapping("/index")
+    public void a(){
+        int a = 0;
+        int b = 3;
+
+    }
+
+}
---
+++ appBoot/shopping-boot-parent/shopping-boot/src/main/java/org/shopping/shoppingApplication.java
@@ -0,0 +1,13 @@
+package org.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class shoppingApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(shoppingApplication.class, args);
+	}
+
+}
