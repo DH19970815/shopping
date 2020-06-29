6d1a03a577ad087570b9ab7a1ab7f8caf100bee3
DH <2192406468@qq.com>
2020-06-29 17:48:34 +0800

    v

--- appBoot/shopping-boot-parent/shopping-boot/src/main/java/org/shopping/modules/boot/controller/index/Index0010Controller.java
+++ appBoot/shopping-boot-parent/shopping-boot/src/main/java/org/shopping/modules/boot/controller/index/Index0010Controller.java
@@ -1,20 +1,42 @@
 package org.shopping.modules.boot.controller.index;
 
 import lombok.extern.slf4j.Slf4j;
+import org.shopping.modules.boot.entity.address;
+import org.shopping.modules.boot.service.addressService;
+import org.shopping.modules.boot.service.logAopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Transient;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.shopping.modules.boot.service.addressService;
 
 @Slf4j
 @RestController
 @RequestMapping("/boot/index0010")
 public class Index0010Controller {
 
-    @GetMapping("/login")
-    public void a(){
-        int a = 1;
+    @Autowired
+    private addressService addressService;
+    @Autowired
+    private org.shopping.modules.boot.service.logAopService logAopService;
 
-
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public void search(){
+//        addressService.searchAddress();
+//        addressService.tx();
+        addressService.getById(1);
     }
+
+    @RequestMapping(value = "/update", method = RequestMethod.GET)
+    public void update(){
+        address address = addressService.getById(1);
+        address.setName("yangyang");
+        addressService.updateAddress(address);
+    }
+
 
 }
--- appBoot/shopping-boot-parent/shopping-boot/src/main/java/org/shopping/shoppingApplication.java
+++ appBoot/shopping-boot-parent/shopping-boot/src/main/java/org/shopping/shoppingApplication.java
@@ -1,14 +1,17 @@
 package org.shopping;
 
-import org.shopping.boot.common.cors.GlobalCorsConfig;
+import org.mybatis.spring.annotation.MapperScan;
+//import org.shopping.boot.common.cors.GlobalCorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 //@Import({GlobalCorsConfig.class})
-@ComponentScan("org.shopping.boot.common.cors")
+//@ComponentScan("org.shopping.boot.common")
+//@MapperScan("org.shopping.modules.boot.mapper")
 public class shoppingApplication {
 	
 	public static void main(String[] args) {
