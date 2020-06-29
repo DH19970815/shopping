c9f1ffa1827249bb6baa0a9a19a5b19bf428406c
DH <2192406468@qq.com>
2020-06-29 17:51:32 +0800

    1

--- appBoot/shopping-boot-parent/shopping-boot-base-common/src/main/java/org/shopping/boot/common/cors/GlobalCorsConfig.java
+++ appBoot/shopping-boot-parent/shopping-boot-base-common/src/main/java/org/shopping/boot/common/cors/GlobalCorsConfig.java
@@ -1,43 +1,43 @@
-package org.shopping.boot.common.cors;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-@Configuration
-public class GlobalCorsConfig {
-	
-	@Bean
-	public CorsFilter corsFilter() {
-        System.out.println("corsFilter................");
-        //1.添加CORS配置信息
-        CorsConfiguration config = new CorsConfiguration();
-        //1) 允许的域,不要写*，否则cookie就无法使用了
-        config.addAllowedOrigin("http://manage.shopping.com");
-        config.addAllowedOrigin("http://www.shopping.com");
-        //2) 是否发送Cookie信息
-        config.setAllowCredentials(true);
-        //3) 允许的请求方式
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
-        // 4）允许的头信息
-        config.addAllowedHeader("*");
-        // 5）此次预检有效时长,有效时长内无需预检直接跨域访问
-        config.setMaxAge(3600L);
-
-        //2.添加映射路径，我们拦截一切请求
-        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
-        configSource.registerCorsConfiguration("/**", config);
-
-        //3.返回新的CorsFilter.
-        return new CorsFilter(configSource);
-	}
-
-}
+//package org.shopping.boot.common.cors;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+//@Configuration
+//public class GlobalCorsConfig {
+//	
+//	@Bean
+//	public CorsFilter corsFilter() {
+//        System.out.println("corsFilter................");
+//        //1.添加CORS配置信息
+//        CorsConfiguration config = new CorsConfiguration();
+//        //1) 允许的域,不要写*，否则cookie就无法使用了
+//        config.addAllowedOrigin("http://manage.shopping.com");
+//        config.addAllowedOrigin("http://www.shopping.com");
+//        //2) 是否发送Cookie信息
+//        config.setAllowCredentials(true);
+//        //3) 允许的请求方式
+//        config.addAllowedMethod("OPTIONS");
+//        config.addAllowedMethod("HEAD");
+//        config.addAllowedMethod("GET");
+//        config.addAllowedMethod("PUT");
+//        config.addAllowedMethod("POST");
+//        config.addAllowedMethod("DELETE");
+//        config.addAllowedMethod("PATCH");
+//        // 4）允许的头信息
+//        config.addAllowedHeader("*");
+//        // 5）此次预检有效时长,有效时长内无需预检直接跨域访问
+//        config.setMaxAge(3600L);
+//
+//        //2.添加映射路径，我们拦截一切请求
+//        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+//        configSource.registerCorsConfiguration("/**", config);
+//
+//        //3.返回新的CorsFilter.
+//        return new CorsFilter(configSource);
+//	}
+//
+//}
